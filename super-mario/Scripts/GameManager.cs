3e0a0f2007a5dbda5cb7a40544f468e19f29f583
Cheese <puellamagiaila@gmail.com>
2026-05-05 17:25:48 -0300

    Feat: Added jumping

--- super-mario/Scripts/GameManager.cs
+++ super-mario/Scripts/GameManager.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class GameManager : Node
 {
-    public int lives, coins;
-   [Export] public float gravity;
+    public int m_lives, m_coins;
+   [Export] public float _gravity;
     public static GameManager Instance;
 
     public override void _Ready()
--- super-mario/Scripts/Player.cs
+++ super-mario/Scripts/Player.cs
@@ -3,18 +3,25 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-    [Export] private float speed;
+    [Export] private float _speed, _jumpForce;
+    [Export] private Timer _coyoteTimer;
 
-    public override void _Process(double delta)
+    private bool CanJump()
     {
-
+        if(IsOnFloor() || !_coyoteTimer.IsStopped())
+            return true;
+        else
+            return false;
     }
+
     public override void _PhysicsProcess(double delta)
     {
         float direction = Input.GetAxis("MoveLeft", "MoveRight");
         if(!IsOnFloor())
-            Velocity = new Vector2(Velocity.X, Velocity.Y + GameManager.Instance.gravity);
-        Velocity = new Vector2(direction * speed, Velocity.Y);
+            Velocity = new Vector2(Velocity.X, Velocity.Y + GameManager.Instance._gravity * (float)delta);
+        if(Input.IsActionPressed("Jump") && CanJump())
+            Velocity = new Vector2(Velocity.X, Velocity.Y - _jumpForce * (float)delta);
+        Velocity = new Vector2(direction * _speed * (float)delta, Velocity.Y);
         MoveAndSlide();
     }
 }
